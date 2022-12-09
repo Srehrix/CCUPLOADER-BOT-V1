d03fa53852dfb2448e076e56bf0fa7945e697682
Sree Hari <98310399+Srehrix@users.noreply.github.com>
2022-12-09 16:16:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,11 @@ from pyrogram import Client as Clinton
 from aiohttp import web
 from plugins import web_server
 
+app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
+
 if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
@@ -17,7 +22,4 @@ if __name__ == "__main__" :
     api_hash=Config.API_HASH,
     plugins=plugins)
     Warrior.run()
-    app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
+    
